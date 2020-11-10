3609be183533dd9d701362ae7ebe5d632f873116
Renan <renan.balen@gmail.com>
2020-11-09 21:47:48 -0300

    091120

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require("express")
+const app = express()
+
+const handlebars = require("express-handlebars")
+const bodyParser = require("body-parser")
+
+const usuario = require("./models/usuario")
+
+//configurar handle bar 
+app.engine('handlebars',handlebars({defaultLayout:'main'}))
+app.search('view engine','handlebars')
+
+//configurar o motor de tamplate handlebar
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
+
+app.get('/algumacoisa',function(req,res){
+     res.send("Olá...antes não era nada, agora estou vivo!!!")
+})
+
+app.get('/login',function(req,res){}
+    
+)
+
+
+
+app.listen(80);
+
+//esse blobo é disparado pelo enviar do fourmulario
+
+app.post('/cadUsuario',function(req,res){
+     usuario.create({
+          nome:req.body.nome,
+          senha:req.body.senha
+     }).then(function(){
+          res.send("Cadastro com Sucesso")
+     }).catch(function(erro){
+          res.send("Erro"+erro)
+     })
+})
