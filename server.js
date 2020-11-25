f83b190a1d91c3449cd59340215d967b7be4b07e
Renan <renan.balen@gmail.com>
2020-11-25 20:06:45 -0300

    versao1

--- server.js
+++ server.js
@@ -8,23 +8,40 @@ const usuario = require("./models/usuario")
 
 //configurar handle bar 
 app.engine('handlebars',handlebars({defaultLayout:'main'}))
-app.search('view engine','handlebars')
-
+app.set('view engine','handlebars')
 //configurar o motor de tamplate handlebar
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())
+
+app.use('/static',express.static(__dirname + '/public'));
 
 app.get('/algumacoisa',function(req,res){
      res.send("Olá...antes não era nada, agora estou vivo!!!")
 })
 
-app.get('/login',function(req,res){}
-    
-)
+app.get('/login',function(req,res){
+     res.render('login')
+})
 
+app.get('/retorno',function(req,res){
+    res.send('alo')
 
+})
 
-app.listen(80);
+app.get('/',function(req,res){
+     res.render('paginainicial')
+})
+
+app.get('/cadastro',function(req,res){
+      res.render('cadastro')
+})
+
+app.get('/doeagora',function(req,res){
+     res.render('doeagora')
+
+})
+
+app.listen(3000);
 
 //esse blobo é disparado pelo enviar do fourmulario
 
@@ -38,3 +55,6 @@ app.post('/cadUsuario',function(req,res){
           res.send("Erro"+erro)
      })
 })
+
+
+
